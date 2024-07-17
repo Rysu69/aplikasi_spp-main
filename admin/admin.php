693fe974b857c02b2d8aadfe95de92e3ee14ee7e
Rysu69 <141984337+Rysu69@users.noreply.github.com>
2024-07-17 14:15:06 +0800

    add information on landing page

--- admin/admin.php
+++ admin/admin.php
@@ -35,7 +35,7 @@ if($_SESSION['level']!='admin') {
     
     <div class="container mt-5">
         
-        <h3>Aplikasi Pembayaran SPP.</h3>
+        <h3>Aplikasi Pembayaran SPP SD Islam AL-Quds.</h3>
         <div class="alert alert-info">
             Anda Login Sebagai <b>ADMINISTRATOR</b> Aplikasi Pembayaran SPP.
         </div>
@@ -43,12 +43,12 @@ if($_SESSION['level']!='admin') {
         <a href="admin.php" class="btn btn-primary"> Administrator</a>
         <a href="admin.php?url=spp" class="btn btn-primary"> SPP</a>
         <!-- <a href="admin.php?url=kelas" class="btn btn-primary"> Kelas</a> -->
-        <a href="admin.php?url=jurusan" class="btn btn-primary"> Jurusan</a>
+        <a href="admin.php?url=jurusan" class="btn btn-primary"> Kelas</a>
         <a href="admin.php?url=siswa" class="btn btn-primary"> Siswa</a>
         <a href="admin.php?url=petugas" class="btn btn-primary"> Petugas</a>
         <a href="admin.php?url=pembayaran" class="btn btn-primary"> Pembayaran</a>
         <a href="admin.php?url=laporan" class="btn btn-primary"> Laporan</a>
-        <a href="admin.php?url=logout" class="btn btn-primary"> Logout</a>
+        <a href="admin.php?url=logout" class="btn btn-danger"> Logout</a>
 
         
         <div class="card mt-2">
@@ -60,7 +60,8 @@ if($_SESSION['level']!='admin') {
             $file = @$_GET['url'];
             if(empty($file)){
                 echo"<h4>Selamat Datang di halaman Administrator. </h4>";
-                echo"Aplikasi Pembayaran SPP digunakan untuk mempermudah dalam mencatat pembayaran siswa / siswi di sekolah";
+                // echo"Aplikasi Pembayaran SPP digunakan untuk mempermudah dalam mencatat pembayaran siswa / siswi di sekolah";
+                include "beranda.php";
             } else {
                 include $file.'.php';
             }
